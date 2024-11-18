140cf5c5d6e2c9f530ef07798465a8afd3da3776
Sloppyan <90444397+Sloppyan@users.noreply.github.com>
2024-11-18 15:49:44 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
   title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
+  author: 'Catherine',
+  email: 'catherinehuang34@gmail.com',
   link: 'https://zuolan.me',
   newsletter: 'Notionic Weekly',
   description: 'A static blog build on top of Notion and Next.js',
